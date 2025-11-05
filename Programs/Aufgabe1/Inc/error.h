a33585c6cd98f67c68773d077c6457bcf893b9e9
Pascal Sendecky <pascalsendecky@haw-hamburg.de>
2025-11-05 19:56:31 +0100

    fehler beim pushen error.h

--- Programs/Aufgabe1/Inc/error.h
+++ Programs/Aufgabe1/Inc/error.h
@@ -1,7 +1,36 @@
-#ifndef ERROR_H
-#define ERROR_H
+/**
+ ******************************************************************************
+ * @file    error.h
+ * @author  Franz Korf
+ *          HAW-Hamburg
+ *          Labor f�r technische Informatik
+ *          Berliner Tor  7
+ *          D-20099 Hamburg
+ * @version 1.0
+ *
+ * @date    17. Juli 2020
+ * @brief   Einfache Error Handler, der User LED1 und LED2 blinken laesst.
+ ******************************************************************************
+ */
+ 
+/**
+  * @brief Einfacher Error Handler, der in einer Endlosschleife 
+	*        die LEDs LED1 und LED2 blinken laesst.
+  * @param  None
+  * @retval None
+  */
 
+#ifndef _ERROR_H
+#define	_ERROR_H
 
-void errorHandle(int v);
+#define ERR_OVERFLOW   -1
+#define ERR_UNDERFLOW  -2
+#define ERR_DIVZERO    -3
+#define ERR_STACKEMPTY -4
+#define ERR_UNKNOWN    -99
+
+extern void handleError(int v);
 
 #endif
+
+// EOF
