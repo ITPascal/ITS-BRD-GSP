4a19b5434d2a1a8e8ab669911f94f28750751bfd
Julius Sawilski <julius.sawilski@gmail.com>
2025-11-13 17:49:13 +0100

    rechner und input.h & .c angelegt

---
+++ Programs/Aufgabe2/Inc/input.h
@@ -0,0 +1,21 @@
+/**
+  * @file encoder.h
+  * @author Julius Sawilski 
+  * @date Nov 2025
+  * @brief Header file of encoder module. 
+  */
+
+#ifndef INPUT_H
+#define INPUT_H
+#include "stm32f4xx_hal.h"
+
+/*
+ ****************************************************************************************
+ *  @brief      This function initialize the output function.
+ *
+ *  @return     void 
+ ****************************************************************************************/
+void initInput(void);
+
+#endif /* OUTPUT_H */
+// EOF
---
+++ Programs/Aufgabe2/Inc/rechner.h
@@ -0,0 +1,14 @@
+/**
+  * @file rechner.h
+  * @author Julius Sawilski 
+  * @date Nov 2025
+  * @brief Header file of encoder module. 
+  */
+
+#ifndef RECHNER_H
+#define RECHNER_H
+#include "stm32f4xx_hal.h"
+
+
+#endif /* OUTPUT_H */
+// EOF
